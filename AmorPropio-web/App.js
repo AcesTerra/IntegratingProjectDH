332966e141474c0d4d6ceeecf754e462e4127eb1
JorgeOrtizV <jorge.ortiz.valerio@gmail.com>
2021-06-26 22:06:12 -0500

    Changing folder structure

---
+++ AmorPropio-web/App.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.get('/', function(req,res){
+    res.send("Hello World!")
+});
+
+app.get('/home', function(req,res){
+    let Path = path.resolve(__dirname,'./views/home.html')
+    res.sendFile(Path)
+});
+
+app.use(express.static('public')) // Define que las busquedas siempre iniciara en public
+
+app.listen(5000,function(req,res){
+    console.log("Server up and running")
+});
