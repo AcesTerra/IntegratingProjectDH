09f696894f50e066ad208e0bcbde7c2f3b19b347
GrimSatsuma298 <leonardovmdc@gmail.com>
2021-07-02 01:35:09 -0500

    add product detail

--- AmorPropio-web/App.js
+++ AmorPropio-web/App.js
@@ -7,8 +7,12 @@ app.get('/', function(req,res){
     res.sendFile(Path)
 });
 
-app.use(express.static('public')) // Define que las busquedas siempre iniciara en public
+app.use(express.static('./public')) // Define que las busquedas siempre iniciara en public
 
 app.listen(5000,function(req,res){
     console.log("Server up and running")
 });
+
+app.get('/detail', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/detail.html'))
+})
