86ae97ce855447ba7fbfbacf52879ee9c873568e
GrimSatsuma298 <leonardovmdc@gmail.com>
2021-07-25 22:49:01 -0500

    CRUD added

--- AmorPropio-web/App.js
+++ AmorPropio-web/App.js
@@ -1,41 +1,28 @@
+// ************ Require's ************
 const express = require('express');
-const app = express();
 const path = require('path');
+const methodOverride =  require('method-override'); // Pasar poder usar los métodos PUT y DELETE
 const mainRouter = require("./routers/mainRouter")
+const productsRouter = require('./routers/productsRouter')
 
-/*app.get('/', function(req,res){
-    let Path = path.resolve(__dirname,'./views/home.html')
-    res.sendFile(Path)
-});*/
+// ************ express() - (don't touch) ************
+const app = express();
 
 const PORT = process.env.PORT || 8080
 
-app.use(express.static('./public')) // Define que las busquedas siempre iniciara en public 
+// ************ Middlewares - (don't touch) ************
+app.use(express.static(path.join(__dirname, './public')));  // Define que las busquedas siempre iniciara en public 
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method')); // Pasar poder pisar el method="POST" en el formulario por PUT y DELETE
+
+// ************ Template Engine - (don't touch) ************
 app.set("view engine", "ejs")
 //app.set("views", "./views")
 
-app.use(mainRouter)
+// ************ Route System require and use() ************
+app.use('/', mainRouter);
+app.use('/products', productsRouter);
 
 app.listen(PORT, () => {
     console.log("Server running on port: " + PORT)
 });
-
-/*app.listen(5000,function(req,res){
-    console.log("Server up and running")
-});
-
-app.get('/detail', (req, res) =>{
-    res.sendFile(path.resolve(__dirname, './views/detail.html'))
-});
-
-app.get('/login', (req, res) =>{
-    res.sendFile(path.resolve(__dirname, './views/login.html'))
-});
-
-app.get('/cart', (req, res)=> {
-    res.sendFile(path.resolve(__dirname, './views/cart.html'))
-});
-
-app.get('/register', (req, res)=> {
-    res.sendFile(path.resolve(__dirname, './views/register.html'))
-});*/
---
+++ AmorPropio-web/routers/productsRouter.js
@@ -0,0 +1,46 @@
+// ************ Require's ************
+const express = require('express');
+const path = require('path');
+const multer  = require('multer');
+const router = express.Router();
+
+// ************ Controller Require ************
+const productsController = require('../controllers/productsController');
+// ************ Multer Storage ************
+var storage = multer.diskStorage({
+	destination: (req, file, cb)=>{
+		cb(null, path.join(__dirname,'../public/images/'))
+	},
+	filename: (req, file, cb) =>{
+
+		cb(null, 'img-'+file.originalname.replace(" ", "-").toLocaleLowerCase())
+	}
+})
+
+
+var upload = multer({storage: storage})
+
+/*** GET ALL PRODUCT ***/ 
+router.get('/', productsController.all);
+
+/*** CREATE ONE PRODUCT ***/
+router.get('/create', productsController.create);
+router.post('/', upload.single("image"), productsController.store)
+
+/*** GET ONE PRODUCT ***/ 
+router.get('/:id', productsController.detail);
+
+/*** EDIT ONE PRODUCT ***/
+router.get('/:id/edit', productsController.edit);
+router.put('/:id', upload.single("image") ,productsController.update);
+
+/*** DELETE ONE PRODUCT ***/
+router.delete('/:id', productsController.destroy)
+
+
+
+
+
+
+
+module.exports = router;
